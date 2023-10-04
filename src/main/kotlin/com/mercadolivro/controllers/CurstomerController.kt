35fa1a88f310fb3c2900b407d3cfc586a7256b96
Victor Lourenço <victorhlo12@hotmail.com>
2023-10-04 13:57:30 -0300

    FEAT: Criando um endpoint para cadastrar um customer

--- src/main/kotlin/com/mercadolivro/controllers/CurstomerController.kt
+++ src/main/kotlin/com/mercadolivro/controllers/CurstomerController.kt
@@ -2,6 +2,8 @@ package com.mercadolivro.controllers
 
 import com.mercadolivro.models.Customer
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,6 +14,11 @@ class CurstomerController {
     @GetMapping
     fun getCustomer(): Customer {
         return Customer(1, "James Gosling", "james.gosling@oracle.com")
+    }
+
+    @PostMapping
+    fun create(@RequestBody customer: Customer) {
+        println(customer)
     }
 
 }
