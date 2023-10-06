a1d8436d11c4df4b83837eb1586f77d24c8395d7
Victor Lourenço <victorhlo12@hotmail.com>
2023-10-06 10:43:31 -0300

    FEAT: Implementando o método findById() no customer

--- src/main/kotlin/com/mercadolivro/controllers/CurstomerController.kt
+++ src/main/kotlin/com/mercadolivro/controllers/CurstomerController.kt
@@ -2,8 +2,10 @@ package com.mercadolivro.controllers
 
 import com.mercadolivro.models.Customer
 import com.mercadolivro.requests.PostCustomerRequest
+import jakarta.websocket.server.PathParam
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,6 +21,11 @@ class CurstomerController {
     @GetMapping
     fun getCustomer(): List<Customer> {
         return customers
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomerById(@PathVariable id: Int): Customer? {
+        return customers.find { x -> x.id == id }
     }
 
     @PostMapping
