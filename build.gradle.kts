3db2c19a270d9f7a33a0c6ad21c0cd93de5d6b9d
Victor Lourenço <victorhlo12@hotmail.com>
2023-10-11 15:40:23 -0300

    CONFIG: Configurando a conexão com a base de dados

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.4")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.postgresql:postgresql:42.6.0")
+
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
