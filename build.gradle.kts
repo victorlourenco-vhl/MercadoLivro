b6309a4d7aedcae7136449685721f5b421b65b03
Victor Lourenço <victorhlo12@hotmail.com>
2023-10-11 15:24:03 -0300

    CONFIG: Adicionando a dependência do MockK e criando o profile de teste

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.13.8")
 }
 
 tasks.withType<KotlinCompile> {
